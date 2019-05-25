c1966c8bc4739082b97f04a6ded027141048e9b4
Maddi <sgdmaddi.97@gmail.com>
2019-05-25 16:45:41 +0200

    setup for ubuntu OS and add README

--- Backend/database_controller.py
+++ Backend/database_controller.py
@@ -1,12 +1,15 @@
+from datetime import datetime
+
 from generate_database import create_database
+
 
 class database_controller:
     def __init__(self):
         self.engine = create_database()
 
     def add_player(self, name):
-        self.engine.execute("INSERT INTO player VALUES(null, '{}')".format(name))
-
+        self.engine.execute(
+            "INSERT INTO player VALUES(null, '{}')".format(name))
 
     def add_game(self, count, date, playerList):
         idList = []
@@ -15,28 +18,29 @@ class database_controller:
             idList.append(self.get_player_id(i))
 
 
-#3-5 player per game
+# 3-5 player per game
 #date, count, p1, p2, p3, ( p4, p5)
-        if(len(idList)==3):
-                self.engine.execute("INSERT INTO game VALUES(null, {},{}, {},{},{}, null, null)".format(date, count, idList[0],idList[1], idList[2]))
-        elif(len(idList)==4):
-                self.engine.execute("INSERT INTO game VALUES(null, {},{}, {},{},{},{}, null)".format(date, count, idList[0],idList[1], idList[2], idList[3]))
-        elif(len(idList)==5):
-                self.engine.execute("INSERT INTO game VALUES(null, {},{}, {},{},{},{},{})".format(date, count, idList[0],idList[1], idList[2], idList[3],idList[4]))
+        if(len(idList) == 3):
+            self.engine.execute("INSERT INTO game VALUES(null, '{}',{}, {},{},{}, null, null)".format(
+                date, count, idList[0], idList[1], idList[2]))
+        elif(len(idList) == 4):
+            self.engine.execute("INSERT INTO game VALUES(null, '{}',{}, {},{},{},{}, null)".format(
+                date, count, idList[0], idList[1], idList[2], idList[3]))
+        elif(len(idList) == 5):
+            self.engine.execute("INSERT INTO game VALUES(null, '{}',{}, {},{},{},{},{})".format(
+                date, count, idList[0], idList[1], idList[2], idList[3], idList[4]))
         else:
-                print("Unknown error occurd in add_game")
-
-
+            print("Unknown error occurd in add_game")
 
     def get_player_id(self, name):
-        result=None
-        id = self.engine.execute("SELECT playerID FROM player WHERE name= '{}' ".format(name))
+        result = None
+        id = self.engine.execute(
+            "SELECT playerID FROM player WHERE name= '{}' ".format(name))
 
         for x in id:
             result = x[0]
 
         return result
-    
 
 
 x = database_controller()
@@ -47,5 +51,6 @@ x.add_player('Friedrich')
 x.add_player('Jakob')
 
 
-x.add_game(5,"01/01/2000", ['Maddi', 'Johann', 'Johan','Friedrich', 'Jakob'])
-x.add_game(36,"01/01/2000", ['Johan','Friedrich', 'Jakob'])
+x.add_game(5, datetime.now(), [
+           'Maddi', 'Johann', 'Johan', 'Friedrich', 'Jakob'])
+x.add_game(36, datetime.now(), ['Johan', 'Friedrich', 'Jakob'])
