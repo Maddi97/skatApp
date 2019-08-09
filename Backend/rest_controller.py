470b7465d26f6c757845deef3b7d6d7dc1c2596a
Maddi <sgdmaddi.97@gmail.com>
2019-08-09 17:10:57 +0200

    setup new db structure

--- Backend/rest_controller.py
+++ Backend/rest_controller.py
@@ -10,29 +10,35 @@ db_controller = database_controller()
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def index():
     return jsonify({'text': "Hello, World!"})
 
-@app.route('/addGame', methods = ['POST'])
+
+@app.route('/addGame', methods=['POST'])
 def add_new_game():
     jsdata = request.json
     print(jsdata)
-    db_controller.add_game(1, datetime.now(), jsdata['playerList'])
+    db_controller.add_game(str(datetime.now()).split(" ")[
+                           0], 1, len(jsdata['playerList']))
     return (jsonify({'success': 'true'}))
 
-@app.route('/addGameDetails', methods = ['POST'])
+
+@app.route('/addGameDetails', methods=['POST'])
 def add_game_details():
     jsdata = request.json
 
     game_data = game_details_datasctrucutre(jsdata)
-
+    gameID = db_controller.get_last_game_id()
+    playerID = db_controller.get_player_id(game_data['Gespielt'])
     print("game_data= {}".format(game_data))
-    db_controller.add_score(game_data['playerID'], game_data['gameID'], game_data['gameNum'], game_data['score'], game_data['color'], game_data['unter'], 
-                           game_data['bock'], game_data['hand'], game_data['schneider'], game_data['schwarz'], game_data['schneider_angesagt'], game_data['schwarz_angesagt'], game_data['ouvert'])
+    db_controller.add_gameDetails(game_data['No'], gameID, playerID, game_data[game_data['Gespielt']], game_data['color'], game_data['unter'],
+                                  game_data['hand'], game_data['schneider'], game_data['schwarz'], game_data['schneider_angesagt'], game_data['schwarz_angesagt'], game_data['ouvert'], game_data['bock'])
     return (jsonify({'success': 'true'}))
 
-@app.route('/addPlayer', methods = ['POST'])
+
+@app.route('/addPlayer', methods=['POST'])
 def add_new_player():
     jsdata = request.json
     db_controller.add_player(jsdata['playerName'])
@@ -40,19 +46,9 @@ def add_new_player():
 
 
 def game_details_datasctrucutre(game_details):
-    # datastructure={'playerID': 0, 'gameID': 0, 'gameNum':0, 'score':0, 'color': '', 'unter': '',
-    #                'bock':False, 'hand': False, 'schneider': False, 'schwarz': False, 'schneider_angesagt': False, 'schwarz_angesagt': False, 'ouvert': False}
+    # TODO
+    return game_details
 
-    # datastructure['playerID']=db_controller.get_player_id(game_details['Gespielt'])
-    # datastructure['gameID']=db_controller.get_last_game_id()
-    # datastructure['gameNum']=game_details['No']
-    # datastructure['score']=game_details[game_details['Gespielt']]
-    # datastructure['color']=game_details['Farbe']
-    # datastructure['unter']=game_details['Unter']
-    datastructure={'playerID': 1, 'gameID': 1, 'gameNum':1, 'score':1010, 'color': 'Eichel', 'unter': 'Mit 2',
-                   'bock':False, 'hand': False, 'schneider': False, 'schwarz': False, 'schneider_angesagt': False, 'schwarz_angesagt': False, 'ouvert': False}
-    return datastructure
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
