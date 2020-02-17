36b87f315c0eb975a7d6b171565f55473b02e5c9
schndrrr <johann.schneider90@hotmail.de>
2020-02-17 17:46:41 +0100

    new components

---
+++ Frontend/src/app/game/charts/charts.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+import { RestComService } from '../../rest-com.service';
+import { data_row } from '../../env'; 
+
+
+@Component({
+  selector: 'app-charts',
+  templateUrl: './charts.component.html',
+  styleUrls: ['./charts.component.css']
+})
+export class ChartsComponent implements OnInit {
+
+  // @Input() table: number[] = [];
+  table: number[];
+  gameData: data_row[];
+  ELEMENT_DATA: data_row[] = [];
+  counter: number = 0;
+
+  tables: any;
+  lineChartData: ChartDataSets[] = [
+    { label: 'Points' },
+  ];
+
+  lineChartLabels: Label[] = [];
+
+  lineChartOptions = {
+    responsive: true,
+  };
+
+  lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,255,0,0.28)',
+    },
+  ];
+
+  lineChartLegend = true;
+  lineChartPlugins = [];
+  lineChartType = 'line';
+  
+  constructor( private restCom: RestComService ) { }
+
+  ngOnInit() {
+    // console.log('1',this.table);
+    // console.log('2',this.lineChartData);
+    // this.restCom.currentGameDetails.subscribe(data => {
+    //   this.ELEMENT_DATA.push(data);
+    //   this.lineChartData = [{ data: this.ELEMENT_DATA[this.counter]['johann'] }];
+    //  });
+    this.restCom.currentGameTable.subscribe(data => {
+      this.table = data; 
+      console.log('3', this.table)
+      this.lineChartData = [{ data: this.table }];
+      this.lineChartLabels.push(this.counter.toString());
+      this.counter++;
+    });
+    }
+}
---
+++ Frontend/src/app/game/table/table.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { MatTableDataSource } from "@angular/material";
+import { RestComService } from '../../rest-com.service';
+
+import {
+  SPECS,
+  COLUMNS,
+  FARBE,
+  UNTER,
+  data_row,
+  INITIAL_DATA_ROW
+} from "../../env";
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  // @Input() dataSource: any;
+  @Input() displayedColumns: string[];
+
+  dataSource: any;
+
+  ngOnChange() {
+  }
+
+
+  constructor(
+    private restCom: RestComService
+  ) { }
+
+  ngOnInit() {
+    this.restCom.currentDataSource.subscribe(data => {
+      this.dataSource = data;
+    });
+  }
+
+
+}
