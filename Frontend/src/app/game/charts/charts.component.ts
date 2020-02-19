b329b859febcfb13590f7f3ba320df6417313cce
Maddi <sgdmaddi.97@gmail.com>
2020-02-19 23:58:02 +0100

    blablbla

--- Frontend/src/app/game/charts/charts.component.ts
+++ Frontend/src/app/game/charts/charts.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ChartDataSets, ChartOptions } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
-import { RestComService } from '../../rest-com.service';
-import { data_row } from '../../env'; 
-
+import { Component, OnInit, Input } from "@angular/core";
+import { ChartDataSets, ChartOptions } from "chart.js";
+import { Color, Label } from "ng2-charts";
+import { RestComService } from "../../rest-com.service";
+import { data_row } from "../../env";
 
 @Component({
-  selector: 'app-charts',
-  templateUrl: './charts.component.html',
-  styleUrls: ['./charts.component.css']
+  selector: "app-charts",
+  templateUrl: "./charts.component.html",
+  styleUrls: ["./charts.component.css"]
 })
 export class ChartsComponent implements OnInit {
-
   // @Input() table: number[] = [];
   table: number[];
   gameData: data_row[];
@@ -19,28 +17,26 @@ export class ChartsComponent implements OnInit {
   counter: number = 0;
 
   tables: any;
-  lineChartData: ChartDataSets[] = [
-    { label: 'Points' },
-  ];
+  lineChartData: ChartDataSets[] = [{ label: "Points" }];
 
   lineChartLabels: Label[] = [];
 
   lineChartOptions = {
-    responsive: true,
+    responsive: true
   };
 
   lineChartColors: Color[] = [
     {
-      borderColor: '#343a40',
-      backgroundColor: 'rgba(255,255,0,0.28)',
-    },
+      borderColor: "#343a40",
+      backgroundColor: "rgba(255,255,0,0.28)"
+    }
   ];
 
   lineChartLegend = true;
   lineChartPlugins = [];
-  lineChartType = 'line';
-  
-  constructor( private restCom: RestComService ) { }
+  lineChartType = "line";
+
+  constructor(private restCom: RestComService) {}
 
   ngOnInit() {
     // console.log('1',this.table);
@@ -49,12 +45,22 @@ export class ChartsComponent implements OnInit {
     //   this.ELEMENT_DATA.push(data);
     //   this.lineChartData = [{ data: this.ELEMENT_DATA[this.counter]['johann'] }];
     //  });
+
+    var dictionary = this.restCom.getAllRoundsOfAllPlayerPerGame();
+    console.log(dictionary);
+    var keyList = Object.keys(dictionary);
+
+    for (var index in keyList) {
+      console.log("player: " + keyList[index]);
+      console.log(keyList[index] + ": " + dictionary[keyList[index]]);
+    }
+
     this.restCom.currentGameTable.subscribe(data => {
-      this.table = data; 
-      console.log('3', this.table)
+      this.table = data;
+      console.log("3", this.table);
       this.lineChartData = [{ data: this.table }];
       this.lineChartLabels.push(this.counter.toString());
       this.counter++;
     });
-    }
+  }
 }
