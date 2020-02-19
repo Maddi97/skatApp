8fc0da236073e63a5a3a2c919d36782ecdf7e587
schndrrr <johann.schneider90@hotmail.de>
2020-02-19 17:11:01 +0100

    added highscoreComponent

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { BodyComponent } from './body/body.component';
+import { HighscoreComponent } from './highscore/highscore.component';
 
-const routes: Routes = [
-  {path: 'game', component: GameComponent},
-  {path: 'main', component: BodyComponent },
-  {path: '', redirectTo: '/main', pathMatch: 'full'}
-];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
