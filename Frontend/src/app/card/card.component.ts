a11005b687442190d794aa75db7f146df62d457a
schndrrr <johann.schneider90@hotmail.de>
2020-02-22 13:50:01 +0100

    some first animations

--- Frontend/src/app/card/card.component.ts
+++ Frontend/src/app/card/card.component.ts
@@ -1,17 +1,46 @@
+
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, transition, state, style, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
+  animations: [
+    trigger('rotate', [
+      state('rotate', style({transform: 'rotateY(180deg)', background: 'white' })),
+      state('rotateBack', style({transform: 'rotateY(-180deg)', background: 'white' })),
+      
+      
+      transition('* => *', animate(400)),
+      transition('* => *', style({ opacity: 0})),
+    ])
+  ]
 })
 export class CardComponent implements OnInit {
   @Input() option: string;
   @Input() source: string;
+
+  ani: string;
+  anima: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  setAni(){
+    this.ani = 'rotate';
+    this.anima = 'opa'
+  }
+
+  mouseOver(){
+    if(this.ani == 'rotate'){
+      this.ani = '';
+    }
+    else {
+      this.ani = 'rotate';
+    }
+  }
 }
