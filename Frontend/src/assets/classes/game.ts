039964215b1fe96e4e9f60e5a772c0c3efac04ff
martin <mmenzel98@web.de>
2020-03-22 23:21:09 +0100

    added classes to frontend

---
+++ Frontend/src/assets/classes/game.ts
@@ -0,0 +1,15 @@
+import { Player } from './player'
+
+export interface IGame {
+    gameID?: number,
+    date: string | Date,
+    gameRoundAmount: number,
+    players: Number[] | Player[]
+}
+
+export class Game implements IGame {
+
+    constructor(readonly gameID: number, readonly date: Date, 
+        readonly gameRoundAmount: number, readonly players: Player[] ) { }
+
+}
---
+++ Frontend/src/assets/classes/player.ts
@@ -0,0 +1,10 @@
+export interface IPlayer {
+    playerID?: number,
+    name: string
+}
+
+export class Player implements IPlayer {
+
+    constructor(readonly playerID: number, readonly name: string) { }
+
+}
