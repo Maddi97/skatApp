2ae68c2b57fddc04cff858af73207186f627fcc3
martin <mmenzel98@web.de>
2020-03-24 19:58:11 +0100

    game map to obs

--- Frontend/src/app/game/game.service.ts
+++ Frontend/src/app/game/game.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Player, IPlayer } from 'src/assets/classes/player';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, of } from 'rxjs';
 import { ApiService } from '../api.service';
 import { Game, IGame } from 'src/assets/classes/game';
 import { shareReplay, tap } from 'rxjs/operators';
@@ -10,20 +10,20 @@ import { shareReplay, tap } from 'rxjs/operators';
 })
 export class GameService {
 
-    storedGames = new BehaviorSubject<Map<number, Game>>(new Map())
+    storedGames = new BehaviorSubject<Map<number, Observable<Game>>>(new Map())
 
     constructor(private api: ApiService) { }
 
     newGame(players: IPlayer[]): Observable<Game> {
         const game: IGame = {
             date: new Date(),
-            players: players
+            players: players,
         }
    
         return this.api.addGame(game).pipe(
             tap(game => {
                 const currentGames = this.storedGames.getValue()
-                currentGames.set(game.gameID, game)
+                currentGames.set(game.gameID, of(game))
             }),
             shareReplay()
         )
