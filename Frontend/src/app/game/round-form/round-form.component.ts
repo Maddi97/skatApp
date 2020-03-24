44e2cd29337ec8503d118f5a65c936b6466cc3c1
Maddi97 <sgdmaddi.97@gmail.com>
2020-03-24 22:55:06 +0100

    form control

--- Frontend/src/app/game/round-form/round-form.component.ts
+++ Frontend/src/app/game/round-form/round-form.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IPlayer, Player } from 'src/assets/classes/player';
+import { IRound, Color, Unter } from 'src/assets/classes/round';
+import {
+  SPECS,
+  COLUMNS,
+  FARBE,
+  UNTER,
+INITIAL_ROUND
+} from "src/assets/classes/round";
 
 @Component({
   selector: 'app-round-form',
@@ -6,10 +15,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./round-form.component.css']
 })
 export class RoundFormComponent implements OnInit {
+  @Input() players: IPlayer[];
+
+  currentRound: IRound = INITIAL_ROUND;
+
+  // constant definitions
+  specs = SPECS;
+  columns = COLUMNS;
+  farbe = FARBE;
+  unter = UNTER;
 
   constructor() { }
 
   ngOnInit() {
+    console.log()
+  }
+  game_select(key, event){
+    console.log(event)
+    console.log(this.currentRound)
   }
 
 }
